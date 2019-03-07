437a9a3e388bd375cac5c6e0789af95064d53218
Medtotech <48196712+Medtotech@users.noreply.github.com>
2019-03-06 23:11:30 -0600

    Add files via upload

---
+++ Guessing Game One.py
@@ -0,0 +1,45 @@
+#Generate a random number between 1 and 9 (including 1 and 9).
+# Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right
+#Extras:
+# Keep the game going until the user types “exit”
+# Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
+import random
+
+random_no = str(random.randint(1, 10))
+counter = 0
+
+while  True:
+    guess = str ( input( "Guess a number from 1 to 9 : "))
+    if guess != "exit":      #conditon to ensure the continuality of the game
+        while True:
+            counter = counter + 1
+            
+            if guess < random_no:
+                print ("Guess too low")
+                break
+
+            elif guess > random_no: 
+                print("Guess too high ")
+                break
+
+            elif guess == random_no:
+                print("CORRECT GUESS!!!!")
+                break
+
+    else:  #condition to end game
+        print('Thank you for playing')
+        break
+
+print ("you guessed " + str(counter) + " times. ")
+
+
+
+
+
+
+
+
+
+
+
